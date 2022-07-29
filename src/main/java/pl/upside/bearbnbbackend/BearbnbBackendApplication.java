319a3ef9de9a7da8292f45ef1a9734479b0bec5d
Radomir Nowacki <kenderpl@gmail.com>
2022-07-29 22:47:35 +0200

    initial commit

---
+++ src/main/java/pl/upside/bearbnbbackend/BearbnbBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.upside.bearbnbbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearbnbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BearbnbBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/upside/bearbnbbackend/BearbnbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.upside.bearbnbbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BearbnbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
