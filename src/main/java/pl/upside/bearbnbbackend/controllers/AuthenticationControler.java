7f6aa26248423ff10dc3430d4c23c6ed3c9a482a
Radomir Nowacki <kenderpl@gmail.com>
2022-07-30 10:51:24 +0200

    Added SLF4J

--- src/main/java/pl/upside/bearbnbbackend/controllers/AuthenticationControler.java
+++ src/main/java/pl/upside/bearbnbbackend/controllers/AuthenticationControler.java
@@ -1,10 +1,12 @@
 package pl.upside.bearbnbbackend.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import pl.upside.bearbnbbackend.model.User;
 
 @RequestMapping("/api/auth")
 @RestController
+@Slf4j
 public class AuthenticationControler {
     @PostMapping("/signin")
     public User signin(@RequestParam String email, @RequestParam String password){
@@ -14,7 +16,9 @@ public class AuthenticationControler {
     @CrossOrigin("*")
     @PostMapping("/signup")
     public User signup(@RequestParam String email, @RequestParam String password){
-        return new User(-1L, email, password, null);
+        User u = new User(-1L, email, password, null);
+        log.info("Created user: " + email);
+        return u;
     }
 
     @GetMapping("/signup")
